91eafa8ba039f54176744002ba395dd83fe8ebe3
Michael Larner <mbl@amazon.com>
2018-08-11 12:13:37 -0700

    Contact Form - TODO: Fix CORS

---
+++ js/contact.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $("#contact-form").submit(function(event) {
+    formArray = $(this).serializeArray();
+    formJSON = {}
+    $.each(formArray, function(){
+      formJSON[this.name] = this.value;
+    });
+    console.log(formJSON);
+
+    var dt = new Date();
+
+    data = {
+      "operation": "create",
+      "tableName": "contact-form",
+      "payload":{
+        "Item":{
+          "date": dt,
+          formJSON
+        }
+      }
+    }
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "crossOrigin": true,
+      "url": "https://om3jjnbeac.execute-api.us-west-2.amazonaws.com/prod/DynamoDBManager",
+      "method": "POST",
+      "headers": {
+        "Content-Type": "application/json",
+        // "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": ""
+        // "Postman-Token": "c62244c3-f6a0-4161-b245-a12752059b52"
+      },
+      "processData": false,
+      "data": data
+    }
+
+    $.ajax(settings)
+      .done(function(response) {
+        console.log(response);
+      });
+    event.preventDefault();
+  });
+});
