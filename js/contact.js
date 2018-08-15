88ab8e4fc8702ab620ac1fed7585b92d7e111153
Michael Larner <mbl@amazon.com>
2018-08-15 16:40:56 -0700

    Working form!

--- js/contact.js
+++ js/contact.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
   $("#contact-form").submit(function(event) {
     formArray = $(this).serializeArray();
     formJSON = {}
-    $.each(formArray, function() {
-      formJSON[this.name] = this.value;
-    });
+
     console.log(formJSON);
 
     var dt = new Date();
@@ -14,28 +12,24 @@ $(document).ready(function() {
       "tableName": "contact-form",
       "payload": {
         "Item": {
-          "date": dt,
-          formJSON
+          "date": dt
         }
       }
-    }
+    };
+
+    $.each(formArray, function() {
+      data["payload"]["Item"][this.name] = this.value;
+    });
+
+    data = JSON.stringify(data);
+
+    console.log(data);
 
     var settings = {
       "async": true,
       "crossDomain": true,
-      "crossOrigin": true,
       "url": "https://om3jjnbeac.execute-api.us-west-2.amazonaws.com/prod/DynamoDBManager",
       "method": "POST",
-      "Content-Type": "application/json",
-      "headers": {
-        "Content-Type": "application/json"
-        // "Access-Control-Request-Methods": "POST",
-        // //   // "Cache-Control": "no-cache",
-        // "Access-Control-Request-Headers": "content-type"
-        // "Access-Control-Allow-Origin": "*",
-        //   "Access-Control-Allow-Methods": ""
-        //   // "Postman-Token": "c62244c3-f6a0-4161-b245-a12752059b52"
-      },
       "processData": false,
       "data": data
     }
